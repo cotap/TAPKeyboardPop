b69d86191d8cdd50a85b1b219eca94fec738aaf1
JP Simard <jp@jpsim.com>
2014-04-24 11:21:36 -0700

    Added UITextView example to showcase the need for a fallback "findKeyboard" method in TAPKeyboardPop

---
+++ TAPKeyboardPopExample/TAPKeyboardPopExample/TAPTextViewViewController.h
@@ -0,0 +1,15 @@
+//
+//  TAPTextViewViewController.h
+//  TAPKeyboardPopExample
+//
+//  Created by JP Simard on 4/24/14.
+//  Copyright (c) 2014 Cotap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TAPTextViewViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UITextView *textView;
+
+@end
